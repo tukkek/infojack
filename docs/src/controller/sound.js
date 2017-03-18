0682b3f3e8c653cfcc711d0724c83a161513042e
Alex Henry <javelinrl@gmail.com>
2017-03-18 17:34:57 -0300

    Updating live site

--- docs/src/controller/sound.js
+++
@@ -1,9 +0,0 @@
-var sound={};
-
-PREFIX=window.location.href.contains('localhost')?'./sounds/':'./infojack/sounds';
-sound.ERROR='Computer Error Alert-SoundBible.com-783113881.mp3';
-
-sound.play=function(file){
-    new Audio(PREFIX+file).play();
-}
-
