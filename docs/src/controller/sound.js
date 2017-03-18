1ea879079642eb5302918e84a529f97dccf1461e
Alex Henry <javelinrl@gmail.com>
2017-03-18 17:59:51 -0300

    Updating live site

--- docs/src/controller/sound.js
+++ docs/src/controller/sound.js
@@ -1,9 +1,8 @@
 var sound={};
 
-PREFIX=window.location.href.contains('localhost')?'./sounds/':'./sounds/';
 sound.ERROR='Computer Error Alert-SoundBible.com-783113881.mp3';
 
 sound.play=function(file){
-    new Audio(PREFIX+file).play();
+    new Audio('./sounds/'+file).play();
 }
 
