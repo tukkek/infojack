de3e00e9fa31eee855246c9d34b2c036dd34f583
Alex Henry <javelinrl@gmail.com>
2017-03-18 17:37:41 -0300

    Updating live site

---
+++ docs/src/controller/sound.js
@@ -0,0 +1,9 @@
+var sound={};
+
+PREFIX=window.location.href.contains('localhost')?'../sounds/':'./../sounds/';
+sound.ERROR='Computer Error Alert-SoundBible.com-783113881.mp3';
+
+sound.play=function(file){
+    new Audio(PREFIX+file).play();
+}
+
