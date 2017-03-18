1e30786c32cdca44f37d2e00ac9b0654b8ecb405
Alex Henry <javelinrl@gmail.com>
2017-03-18 17:56:49 -0300

    Adding a timestamp hack in the release build

--- release.sh
+++ release.sh
@@ -7,6 +7,9 @@ fi
 rm -r docs
 mv build/default docs
 rm -r build
+timestamp=`date "+%Y%m%d-%H%M"`
+sed -i "s/dev build/build $timestamp/g" docs/index.html
+exit
 git add docs
 git add docs/**
 git commit -m "Updating live site"
