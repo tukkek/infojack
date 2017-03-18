9949039021a460af2e0ef77504713f156ff6eb1e
Alex Henry <javelinrl@gmail.com>
2017-03-18 17:42:42 -0300

    Updating live site

--- docs/src/controller/sound.js
+++ docs/src/controller/sound.js
@@ -1,6 +1,6 @@
 var sound={};
 
-PREFIX=window.location.href.contains('localhost')?'../sounds/':'./sounds/';
+PREFIX=window.location.href.contains('localhost')?'../sounds/':'./infojack/sounds/';
 sound.ERROR='Computer Error Alert-SoundBible.com-783113881.mp3';
 
 sound.play=function(file){
