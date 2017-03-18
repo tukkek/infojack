cd02215ac95dd45b65321bd3d6d0e7b0c60fa68f
Alex Henry <javelinrl@gmail.com>
2017-03-18 17:35:28 -0300

    fixing path issues

--- src/controller/sound.js
+++ src/controller/sound.js
@@ -1,8 +1,9 @@
 var sound={};
 
+PREFIX=window.location.href.contains('localhost')?'../sounds/':'./../sounds/';
 sound.ERROR='Computer Error Alert-SoundBible.com-783113881.mp3';
 
 sound.play=function(file){
-    new Audio('../../sounds/'+file).play();
+    new Audio(PREFIX+file).play();
 }
 
