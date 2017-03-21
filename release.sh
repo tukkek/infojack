e3712e4621842b1157b3c23c4ae701756b3b4774
Alex Henry <javelinrl@gmail.com>
2017-03-20 22:53:57 -0300

    Adding updated release.sh

--- release.sh
+++ release.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-rm -r build
-polymer build --add-service-worker
-if [ ! -d "build" ]; then
-  exit
+au build --env prod
+if [ $? -ne 0 ]; then
+    exit
 fi
 rm -r docs
-mv build/default docs
-rm -r build
+mkdir docs
+cp -r scripts docs
+cp -r fonts docs
+cp -r images docs
+cp -r sounds docs
+cp index.html docs
+mkdir docs/src
+cp src/*.css docs/src
 timestamp=`date "+%Y%m%d-%H%M"`
 sed -i "s/dev build/build $timestamp/g" docs/index.html
 git add docs
