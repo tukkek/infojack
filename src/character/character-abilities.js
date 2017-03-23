be99b5b151023cc8317ca28be7d51802dd7ddaab
Alex Henry <alex@manaburn>
2017-03-23 18:02:35 -0300

    Ability screen

--- src/character/character-abilities.js
+++ src/character/character-abilities.js
@@ -1,4 +1,20 @@
+/* TODO add difficulty modes */
 export class CharacterAbilities {
     constructor() {
+        this.strength=8;
+        this.dexterity=8;
+        this.constitution=8;
+        this.intelligence=8;
+        this.wisdom=8;
+        this.charisma=8;
+        this.pointbuy=15;
+        this.pointextra=0;
+    }
+    
+    upgrade(ability){
+        if(this[ability]==8&&amount<0){
+            return;
+        }
+        this[ability]+=amount;
     }
 }
