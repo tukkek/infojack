465537ba0476fe1f679b69176c380dd5eac6015d
Alex Henry <tukkek@gmail.com>
2018-06-29 20:38:25 -0300

    Adding Control implementation ideas

--- src/modules/cyberspace/avatar/control.js
+++ src/modules/cyberspace/avatar/control.js
@@ -1,12 +1,17 @@
 import {Avatar} from './avatar';
 
+var OUTCOMES=2;
+
 export class Control extends Avatar{
   constructor(system){
     super('nodes/core.png',system);
     this.tooltip='Process';
+    this.memory=rpg.chancein(OUTCOMES);
+    this.privilege=rpg.chancein(OUTCOMES);
   }
   
   scan(){
+    if(!this.memory&&!this.privilege) this.node.remove(this);
     this.setimage('nodes/control.png');
     this.tooltip='Control';
   }
